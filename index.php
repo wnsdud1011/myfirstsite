900656bc3cf0665eb7a67175a0057819a7c82712
wnsdud1011 <wnsdud088412@gmail.com>
2025-07-05 20:14:47 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>내 첫 PHP</title>
+</head>
+<body>
+  <h1>안녕하세요!</h1>
+  <p>오늘은 <?php echo date("Y-m-d"); ?> 입니다.</p>
+  <p>hello, my name is park_junyoung</p>
+</body>
+</html>
